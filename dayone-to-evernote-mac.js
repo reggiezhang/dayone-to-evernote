2222ca41fe5933d2960afb5a3bc79726dfa77a16
Reggie Zhang <reggiezhang@users.noreply.github.com>
2017-05-08 22:23:26 +0800

    Update dayone-to-evernote-mac.js

--- dayone-to-evernote-mac.js
+++ dayone-to-evernote-mac.js
@@ -7,24 +7,14 @@
  * 
  */
 
-console.time("dayone-to-evernote-mac.js")
-
-/*
-0: /usr/local/Cellar/node/7.10.0/bin/node
-1: /Users/i070159/scripts/dayone-to-evernote-mac.js
-2: /Users/i070159/Desktop/Journal_dayone@20170506/
-*/
+//console.time("dayone-to-evernote-mac.js")
 
 if (process.argv.length < 3) {
     console.log("usage: dayone-to-evernote-mac.js  <dayone_folder>");
     return;
 }
 
-function plist2json(plistPath) {
-    return plist.parse(fs.readFileSync(plistPath, 'utf8'));
-}
-
-var DAYONE_DIR = process.argv[2]; //'/Users/i070159/Desktop/Journal_dayone@20170506/';
+var DAYONE_DIR = process.argv[2]; 
 
 var notebookName = 'Dayone: ' + new Date().toDateString();
 
@@ -35,10 +25,22 @@ var entriesPath = DAYONE_DIR + '/entries/';
 var photosPath = DAYONE_DIR + '/photos/';
 var files = fs.readdirSync(entriesPath);
 
-var count = 0;
-files.forEach(function (filename) {
-    var obj = plist2json(entriesPath + filename);
-    var noteTempFilePath =`${__dirname}/${obj['UUID']}.json`;
+// var count = 0;
+
+var ProgressBar = require('progress');
+console.log();
+var bar = new ProgressBar('Importing [:bar] :current/:total  :percent  elapsed: :elapseds  eta: :etas', {
+    complete: '=',
+    incomplete: ' ',
+    width: 40,
+    total: files.length
+});
+
+var forEach = require('async-foreach').forEach;
+forEach(files, function (filename) {
+    var done = this.async();
+    var obj = plist.parse(fs.readFileSync(entriesPath + filename, 'utf8'));
+    var noteTempFilePath = `${__dirname}/${obj['UUID']}.json`;
     var photoPath = photosPath + obj['UUID'] + '.jpg';
     if (fs.existsSync(photoPath)) {
         obj['Photo Path'] = photosPath + obj['UUID'] + '.jpg';
@@ -52,14 +54,16 @@ files.forEach(function (filename) {
     try {
         var shellCmd = `${__dirname}/create-EN-note-mac.js '${noteTempFilePath}' '${notebookName}' `;
         execSync(shellCmd, { stdio: [0, 1, 2] });
-        count++;
+        // count++;
 
     } catch (e) {
         console.log(e);
     } finally {
         fs.unlinkSync(noteTempFilePath);
+        setTimeout(done, 1);
+        bar.tick(1);
     }
 });
 
-console.log("%d Notes created.", count);
-console.timeEnd("dayone-to-evernote-mac.js");
+//console.log("%d Notes created.", count);
+//console.timeEnd("dayone-to-evernote-mac.js");
