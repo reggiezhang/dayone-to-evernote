535311fad0dd834a7b00b27a8974a53b796beeef
Reggie Zhang <reggie.zhang@sap.com>
2017-05-14 17:07:50 +0800

    commit by git-sync.sh

--- dayone-to-evernote.js
+++ dayone-to-evernote.js
@@ -39,33 +39,84 @@ function prepareDoJsonFile(doPath, filename) {
     return doJsonFilePath;
 }
 
+function getDoNote(doPath, filename) {
+    const fs = require('fs');
+    const plist = require('plist');
+    var doNote = plist.parse(fs.readFileSync(`${getEntriesPath(doPath)}/${filename}`, 'utf8'));
+    var photoPath = `${getPhotosPath(doPath)}${doNote['UUID']}.jpg`;
+    if (fs.existsSync(photoPath)) doNote['Photo Path'] = photoPath;
+    if (doNote['Tags'] == undefined) doNote['Tags'] = new Array();
+    doNote['Tags'][doNote['Tags'].length] = 'dayone';
+    return doNote;
+}
+
+function preparePrarmsFile(doPath, filename, notebookName) {
+    var doNote = getDoNote(doPath, filename);
+    var params = new Object();
+    params.withText = doNote['Entry Text'];
+    params.title = getNoteTitle(params.withText); 
+    params.notebook = notebookName;
+    params.tags = doNote['Tags'];
+    params.created = new Date(doNote['Creation Date']);
+    if (doNote['Photo Path']) {
+        params.attachments = [doNote['Photo Path']];
+    }
+
+    const uuidV4 = require('uuid/v4');
+    const fs = require('fs');
+    const os = require('os');
+    var paramsFilePath = `${os.tmpdir()}/${uuidV4()}.json`;
+    fs.writeFileSync(paramsFilePath, JSON.stringify(params));
+    return paramsFilePath;
+}
+
+function getNoteTitle(noteText) {
+    return noteText.split('\n')[0];
+}
+
+function getEntries(doPath, afterDate) {
+    var entriesPath = getEntriesPath(doPath);
+    var entriesDir = require('fs').readdirSync(entriesPath);
+    if (afterDate === undefined) return entriesDir;
+    var entries = entriesDir.filter(function (item) {
+        var fs = require('fs');
+        var plist = require('plist');
+        var obj = plist.parse(fs.readFileSync(`${getEntriesPath(doPath)}/${item}`, 'utf8'));
+        var noteDate = new Date(obj['Creation Date']);
+        return noteDate > afterDate;
+    });
+    return entries;
+}
+
 function main(argv) {
+    const evernote = require('evernote-jxa');
     var program = require('commander');
     program
-        .version('0.1.3')
-        .usage("<Journal_dayone_dir>")
+        .version('0.1.4')
+        .option('-a, --after <date>', 'date with ISO8601 format. e.g. 2016-05-10T03:08:07+08:00', Date.parse)
         .arguments('<Journal_dayone_dir>')
         .parse(argv);
-    if (!program.args.length) program.help();
+    if (!program.args.length
+        || program.after !== undefined && isNaN(program.after)) program.help();
 
     var notebookName = `Dayone: ${new Date().toDateString()}`;
     var doPath = program.args[0];
     var entriesPath = getEntriesPath(doPath);
 
     var fs = require('fs');
-    var entriesDir = fs.readdirSync(entriesPath);
-    var bar = initProgressBar(entriesDir.length);
-    require('async-foreach').forEach(entriesDir, function (filename) {
+    var entries = getEntries(doPath, program.after);
+    var bar = initProgressBar(entries.length);
+    evernote.createNotebook(notebookName);
+    require('async-foreach').forEach(entries, function (filename) {
         bar.tick(1);
         var done = this.async();
-        var doJsonFilePath = prepareDoJsonFile(doPath, filename);
+        var paramsFilePath = preparePrarmsFile(doPath, filename, notebookName);
         try {
-            var shellCmd = `${__dirname}/lib/create-EN-note-mac.js '${doJsonFilePath}' '${notebookName}'`;
-            require('child_process').execSync(shellCmd, { stdio: [0, 1, 2] });
+            evernote.createNote(paramsFilePath)
         } catch (e) {
             console.log(e);
         } finally {
-            fs.unlinkSync(doJsonFilePath);
+            fs.unlinkSync(paramsFilePath);
             setImmediate(done);
         }
     });
